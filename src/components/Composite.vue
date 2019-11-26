b307a518f4d64022ac044fa4cf7feae5abe543c3
mw92.park <mw92.park@samsung.com>
2019-11-26 21:00:11 +0900

    Create composite template

--- src/components/Composite.vue
+++ src/components/Composite.vue
@@ -1,15 +1,67 @@
 <template>
-    <div>
-        Composite
-    </div>
+  <v-card outlined elevation="1">
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="headline mb-1">Composite</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-row justify="space-around">
+      <v-switch class="ma-2" label="타이틀"></v-switch>
+      <v-switch class="ma-2" label="설명"></v-switch>
+      <v-switch class="ma-2" label="요소 리스트"></v-switch>
+    </v-row>
+    <v-row justify="space-around">
+      <v-switch class="ma-2" label="이미지"></v-switch>
+      <v-switch class="ma-2" label="버튼"></v-switch>
+    </v-row>
+    <v-card-text class="sub-title">
+      <p class="sub-title-value">타이틀</p>
+    </v-card-text>
+    <v-textarea class="text-content-value" counter="200" maxlength="200"></v-textarea>
+    <v-card-text class="sub-title">
+      <p class="sub-title-value">설명</p>
+    </v-card-text>
+    <v-textarea class="text-content-value" counter="1000" maxlength="1000"></v-textarea>
+    <v-card-text class="sub-title">
+      <p class="sub-title-value">이미지URL</p>
+    </v-card-text>
+    <v-text-field
+      class="text-content-value"
+    ></v-text-field>
+  </v-card>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style scoped>
+.sub-title {
+  padding: 0 18px 0 18px;
+  margin: 0 0 0 0;
+  font-size: 13px;
+}
 
+.sub-title-value {
+  margin: 0;
+}
+
+.text-content-value {
+  padding: 0 18px 0 18px;
+  margin: 0;
+}
+
+.infomation {
+  font-size: 14px;
+  margin: 0 18px 18px 18px;
+  color: rgba(0, 0, 0, 0.5);
+}
+
+.add-composite-button {
+  margin: 0 24px 24px 24px;
+}
+
+.composite {
+  margin: 0 24px 24px 24px;
+}
 </style>
--- src/components/CompositeContent.vue
+++ src/components/CompositeContent.vue
@@ -1,15 +1,58 @@
 <template>
-    <div>
-        Composite Content
-    </div>
+  <v-card class="mx-auto" outlined elevation="1">
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="headline mb-1">Composite Content</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <ul class="infomation">
+      <li>compositeContent에 composite을 두 개 이상 넣어서 캐로셀(Carousel)을 구성할 수 있습니다.</li>
+      <li>최대 10개의 Composite를 추가할 수 있습니다.</li>
+      <li>하나의 composite에 "타이틀", "설명", "요소 리스트" 중 적어도 한 개의 속성은 존재해야 합니다.</li>
+      <li>하나의 composite에 "타이틀", "설명", "요소 리스트", "이미지", "버튼" 중 적어도 두 개의 속성이 존재해야 합니다.</li>
+    </ul>
+    <v-btn small color="primary" class="add-composite-button">+카드 추가</v-btn>
+    <Composite class="composite"></Composite>
+  </v-card>
 </template>
 
 <script>
-    export default {
-        
-    }
+import Composite from "./Composite";
+
+export default {
+    components: {
+        Composite,
+    },
+};
 </script>
 
 <style scoped>
+.sub-title {
+  padding: 0 18px 0 18px;
+  margin: 0 0 0 0;
+  font-size: 13px;
+}
 
+.sub-title-value {
+  margin: 0;
+}
+
+.text-content-value {
+  padding: 0 18px 0 18px;
+  margin: 0;
+}
+
+.infomation {
+  font-size: 14px;
+  margin: 0 18px 18px 18px;
+  color: rgba(0, 0, 0, 0.5);
+}
+
+.add-composite-button {
+    margin: 0 24px 24px 24px;
+}
+
+.composite {
+    margin: 0 24px 24px 24px;
+}
 </style>
