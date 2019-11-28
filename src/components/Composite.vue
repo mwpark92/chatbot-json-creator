0d0cf6122b244baf8b16f79129ebc63a4e6ad67e
mwpark92 <alsdnr773@gmail.com>
2019-11-29 00:04:04 +0900

    Add switch change event handler

--- src/components/Composite.vue
+++ src/components/Composite.vue
@@ -5,44 +5,31 @@
         <v-layout>
           <v-list-item-title class="headline mb-1">Composite</v-list-item-title>
           <div class="close-icon">
-            <v-btn @click="onClickDeleteIcon">mdi-close</v-btn>
-          </div>
-          <!-- <div class="close-icon">
             <v-icon @click="onClickDeleteIcon">mdi-close</v-icon>
-          </div>-->
+          </div>
         </v-layout>
       </v-list-item-content>
     </v-list-item>
     <v-row justify="space-around">
-      <v-switch class="ma-2" label="타이틀" v-model="isTitleOn"></v-switch>
-      <v-switch class="ma-2" label="설명" v-model="isDescriptionOn"></v-switch>
-      <v-switch class="ma-2" label="요소 리스트" v-model="isElementListOn"></v-switch>
+      <v-switch class="ma-2" label="타이틀" v-model="isTitleOn" @change="onChangeTitleSwitch"></v-switch>
+      <v-switch class="ma-2" label="설명" v-model="isDescriptionOn" @change="onChangeDescriptionSwitch"></v-switch>
+      <v-switch class="ma-2" label="요소 리스트" v-model="isElementListOn" @change="onChangeElementListSwitch"></v-switch>
     </v-row>
     <v-row justify="space-around">
-      <v-switch class="ma-2" label="이미지" v-model="isImageOn"></v-switch>
-      <v-switch class="ma-2" label="버튼" v-model="isButtonListOn"></v-switch>
+      <v-switch class="ma-2" label="이미지" v-model="isImageOn" @change="onChangeImageSwitch"></v-switch>
+      <v-switch class="ma-2" label="버튼" v-model="isButtonListOn" @change="onChangeButtonListSwitch"></v-switch>
     </v-row>
     <div v-if="isTitleOn">
       <v-card-text class="sub-title">
         <p class="sub-title-value">타이틀</p>
       </v-card-text>
-      <v-textarea
-        class="text-content-value"
-        counter="200"
-        maxlength="200"
-        v-model="compositeData.title"
-      ></v-textarea>
+      <v-textarea class="text-content-value" counter="200" maxlength="200" v-model="compositeData.title"></v-textarea>
     </div>
     <div v-if="isDescriptionOn">
       <v-card-text class="sub-title">
         <p class="sub-title-value">설명</p>
       </v-card-text>
-      <v-textarea
-        class="text-content-value"
-        counter="1000"
-        maxlength="1000"
-        v-model="compositeData.description"
-      ></v-textarea>
+      <v-textarea class="text-content-value" counter="1000" maxlength="1000" v-model="compositeData.description"></v-textarea>
     </div>
     <div v-if="isImageOn">
       <v-card-text class="sub-title">
@@ -92,7 +79,7 @@ export default {
         image: {
           imageUrl: ""
         },
-        buttonLIst: []
+        buttonList: []
       }
     };
   },
@@ -101,17 +88,43 @@ export default {
       deep: true,
       immediate: true,
       handler: function(newValue) {
-        const trimmedComposite = this.trimComposite(newValue);
-        this.$emit("change-composite", {
-          composite: trimmedComposite,
-          index: this.index
-        });
+        this.compositeUpdate(newValue);
       }
     }
   },
   methods: {
     onClickDeleteIcon() {
       this.$store.commit("deleteComposite", this.index);
+    },
+    onChangeTitleSwitch() {
+      if (!this.isTitleOn) {
+        this.compositeData.title = "";
+      }
+      this.compositeUpdate(this.compositeData);
+    },
+    onChangeDescriptionSwitch() {
+      if (!this.isDescriptionOn) {
+        this.compositeData.description = "";
+      }
+      this.compositeUpdate(this.compositeData);
+    },
+    onChangeElementListSwitch() {
+      if (!this.isElementListOn) {
+        this.compositeData.elementList = [];
+      }
+      this.compositeUpdate(this.compositeData);
+    },
+    onChangeImageSwitch() {
+      if (!this.isImageOn) {
+        this.compositeData.image.imageUrl = "";
+      }
+      this.compositeUpdate(this.compositeData);
+    },
+    onChangeButtonListSwitch() {
+      if (!this.isTitleOn) {
+        this.compositeData.buttonList = [];
+      }
+      this.compositeUpdate(this.compositeData);
     },
     trimComposite(compositeData) {
       let results = {};
@@ -130,10 +143,17 @@ export default {
         results.image.imageUrl = compositeData.image.imageUrl;
       }
       if (this.isButtonListOn) {
-        results.buttonLIst = compositeData.buttonLIst;
+        results.buttonList = compositeData.buttonList;
       }
 
       return results;
+    },
+    compositeUpdate(newValue) {
+      const trimmedComposite = this.trimComposite(newValue);
+      this.$emit("change-composite", {
+        composite: trimmedComposite,
+        index: this.index
+      });
     }
   }
 };
